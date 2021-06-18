b1faaa913376d82d901a9caf1afc69c50251f790
abhishek-msit-032 <abhishekbejavada@msitprogram.net>
2021-06-18 22:45:03 +0530

    first commit

---
+++ rasa_pro-main - working/frontend/src/Home.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import styled from 'styled-components';
+import Logo from './images/msitadv.jpg';
+import './Home.css'
+const GridWrapper = styled.div`
+  display: grid;
+  grid-gap: 10px;
+  margin-top: 1em;
+  margin-left: 6em;
+  margin-right: 6em;
+  grid-template-columns: repeat(12, 1fr);
+  grid-auto-rows: minmax(25px, auto);
+`;
+export const Home = (props) => (
+  <GridWrapper>
+    <img src={Logo} width="400" height="400" alt="logo" />
+    
+  </GridWrapper>
+)
